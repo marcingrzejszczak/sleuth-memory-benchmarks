aab5878a47304a69566e2f758151f7b75b43c2be
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-02-16 18:43:51 +0100

    WIP

--- non-sleuth-application/src/main/java/com/example/NonSleuthApplication.java
+++ non-sleuth-application/src/main/java/com/example/NonSleuthApplication.java
@@ -1,12 +1,38 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class NonSleuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NonSleuthApplication.class, args);
+	}
+
+	@Autowired RestTemplate restTemplate;
+	@Value("${server.port}") int port;
+
+	@GetMapping("/test")
+	long test() {
+		System.out.println("Calling myself");
+		return this.restTemplate.getForObject("http://localhost:" + this.port + "/totalMemory", Long.class);
+	}
+
+	@GetMapping("/totalMemory")
+	long totalMemory() {
+		System.gc();
+		return Runtime.getRuntime().totalMemory();
+	}
+
+	@Bean RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
